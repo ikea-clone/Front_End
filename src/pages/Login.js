9d68827bb21516a2e47050af5700d2026580ebff
parkdaxun <parkde0207@gmail.com>
2022-09-22 14:40:16 +0900

    [add] login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css';
 import Cup from "../img/cup.png";
 
 const Login = () => {
+    let [id, setId] = useState('');
+    let [pw, setPw] = useState('');
+
     return (
         <div className="background">
             <img className='cup' src={Cup}/>
-            <text>Welcome to PIKU</text>
+            <div className="title">
+                <text>Welcome to PIKU</text>
+            </div>
+            <br/>
             <div className="say">
                 <text>이상형 월드컵을 만들고 공유하려면 로그인이 필요합니다.</text>
                 <br />
+                <br />
                 <text>이메일 주소만으로 간단히 계정 생성이 가능합니다.</text>
+            </div>
+            <br />
+            <div className="put">
+                <div className="email">
+                    <input
+                        placeholder="이메일"
+                        id="id"
+                        className="ilogin"
+                    />
+                </div>
+                <br />
+                <div className="password">
+                    <input
+                        type="password"
+                        placeholder="비밀번호"
+                        id="password"
+                        className="ilogin"
+                    />
+                </div>
             </div>
         </div>
     );
