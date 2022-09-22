76c7b569344fe1e526e2a1e87eda69fd87e7833f
parkdaxun <parkde0207@gmail.com>
2022-09-22 13:44:37 +0900

    check

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,8 +7,11 @@ const Login = () => {
         <div className="background">
             <img className='cup' src={Cup}/>
             <text>Welcome to PIKU</text>
-            <text>이상형 월드컵을 만들고 공유하려면 로그인이 필요합니다.</text>
-            <text>이메일 주소만으로 간단히 계정 생성이 가능합니다.</text>
+            <div className="say">
+                <text>이상형 월드컵을 만들고 공유하려면 로그인이 필요합니다.</text>
+                <br />
+                <text>이메일 주소만으로 간단히 계정 생성이 가능합니다.</text>
+            </div>
         </div>
     );
 }
