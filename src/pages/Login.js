24a329dfa53a94740715624a2ad17da7e7c3d87c
KJS <digyrh456789@gmail.com>
2022-10-27 08:12:21 +0900

    Just

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
             </div>
             <br/>
             <div className="say">
-                <text>이상형 월드컵을 만들고 공유하려면 로그인이 필요합니다.</text>
+                <text>이상형 월드컵을 만들고 공유하려면 로그인이 필요합니다..</text>
                 <br />
                 <br />
                 <text>이메일 주소만으로 간단히 계정 생성이 가능합니다.</text>
