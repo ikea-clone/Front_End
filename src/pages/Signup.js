16503322a83fb78906a069e58172f58f58f57b22
KJS <digyrh456789@gmail.com>
2022-10-26 20:16:57 +0900

    Sign up

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,15 +1,34 @@
 import React, {useState} from 'react';
 import './Signup.css';
 import Cup from "../img/cup.png";
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+    const nav = useNavigate();
     let [na, setna] = useState('');
     let [id, setId] = useState('');
     let [pw, setPw] = useState('');
-
+    function main(){
+        alert('로그인에 성공하셨습니다!')
+        nav('/');
+    }
+    function logins() {
+        nav('/login')
+    }
     return (
         <div>
             <img className='cup' src={Cup}/>
+            <div className="title">
+                <text>Welcome to PIKU</text>
+            </div>
+            <br/>
+            <div className="say">
+                <text>이상형 월드컵을 만들고 공유하려면 로그인이 필요합니다.</text>
+                <br />
+                <br />
+                <text>이메일 주소만으로 간단히 계정 생성이 가능합니다.</text>
+            </div>
+            <br />
             <div className="signin">
                 <input
                     placeholder="닉네임"
@@ -29,7 +48,8 @@ const Signup = () => {
                     className="password"
                 />
                 <br/>
-                <button>하이</button>
+                <button className='register' onClick={main}>계정생성</button><br/>
+                <button className='golog' onClick={logins}>로그인</button>
             </div>
         </div>
     );
